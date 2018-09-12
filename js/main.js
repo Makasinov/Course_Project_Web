4835e6c666e44404f64c7171f2cee5220e241e6b
Ilya <31817995+Makasinov@users.noreply.github.com>
2018-09-13 02:59:54 +0300

    fc

---
+++ js/main.js
@@ -0,0 +1,28 @@
+function login() {
+	var login = document.getElementById('loginEdit').value;
+	var password = document.getElementById('passwordEdit').value;
+	alert('Login: ' + login + '\nPassword: ' + password);
+}
+
+function register() {
+	alert('open register');
+}
+
+function listOfLessons() {
+	alert('list of lessons');
+}
+
+function loginButton() {
+    document.getElementById('reg_form').style.display = "block";
+    document.querySelector
+}
+
+function close() {
+	document.getElementById('reg_form').style.display = 'none';
+}
+
+function ready() {
+    close();
+}
+
+document.addEventListener("DOMContentLoaded", ready);
