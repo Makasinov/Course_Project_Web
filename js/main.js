9153c3f787122a514fe8fcea1e74beec26b5c3ae
Ilya <31817995+Makasinov@users.noreply.github.com>
2018-09-13 23:15:03 +0300

    Добавлен список уроков

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var iframe = document.getElementById('_iframe');
+
 function login() {
 	var login = document.getElementById('loginEdit').value;
 	var password = document.getElementById('passwordEdit').value;
@@ -9,7 +11,10 @@ function register() {
 }
 
 function listOfLessons() {
-	alert('list of lessons');
+	var list_of_lessons = document.getElementById('list_of_lessons');
+    if(list_of_lessons.style.display == 'none')
+        list_of_lessons.style.display = 'block';
+    else list_of_lessons.style.display = 'none';
 }
 
 function loginButton() {
@@ -19,10 +24,21 @@ function loginButton() {
 
 function close() {
 	document.getElementById('reg_form').style.display = 'none';
+    document.getElementById('list_of_lessons').style.display = 'none';
+    document.getElementById('choose_a').style.display = 'none';
 }
 
 function ready() {
     close();
+    document.getElementById('choose_a').style.display = 'block';
+    var test_a = document.getElementById('test_a');
+        
+    var innerDoc = (iframe.contentDocument) ? iframe.contentDocument : iframe.contentWindow.document;
+                
+    //test_a.innerHTML = innerDoc;
+        
+    var doc_height = window.innerHeight - 70;
+    document.getElementById("main_content").style.height = doc_height + 'px';
 }
 
 document.addEventListener("DOMContentLoaded", ready);
