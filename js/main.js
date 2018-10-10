93491098b9d2643e5645f07628f55308fd819638
Ilya <31817995+Makasinov@users.noreply.github.com>
2018-10-10 20:26:46 +0300

    Некоторая проделанная работа после ремонта ноутбука

--- js/main.js
+++ js/main.js
@@ -1,4 +1,92 @@
-var iframe = document.getElementById('_iframe');
+//var sourcePage = "";
+
+function setiframe(sourcePage) {
+    var iframe = document.getElementsByTagName('iframe')[0];
+    iframe.src = sourcePage;
+}
+
+function action_1() {
+    close();
+    setiframe("https://code-live.ru/post/cpp-hello-world");
+}
+
+function action_2() {
+    close();
+    setiframe("https://code-live.ru/post/cpp-variables-and-datatypes/");
+}
+
+function action_3() {
+    close();
+    setiframe("https://code-live.ru/post/cpp-if-else/");
+}
+
+function action_4() {
+    close();
+    setiframe("https://code-live.ru/post/cpp-loops/");
+}
+
+function action_5() {
+    close();
+    setiframe("https://code-live.ru/post/cpp-arrays/");
+}
+
+function action_6() {
+    close();
+    setiframe('https://code-live.ru/post/cpp-functions/');
+}
+
+function action_7() {
+    close();
+    setiframe('https://code-live.ru/post/cpp-pointers/');
+}
+
+function action_8() {
+    close();
+    setiframe('https://code-live.ru/post/cpp-dynamic-arrays/');
+}
+
+function action_9() {
+    close();
+    setiframe('https://code-live.ru/post/cpp-command-line-arguments/');
+}
+
+function action_10() {
+    close();
+    setiframe('https://code-live.ru/post/cpp-classes/');
+}
+
+function action_11() {
+    close();
+    setiframe('https://code-live.ru/post/cpp-continue-learning-classes/');
+}
+
+function action_12() {
+    close();
+    setiframe('https://code-live.ru/post/cpp-vector/');
+}
+
+function action_13() {
+    close();
+    setiframe('https://code-live.ru/post/cpp-class-inheritance/');
+}
+
+function action_14() {
+    close();
+    setiframe('https://code-live.ru/post/cpp-function-overload/');
+}
+
+function action_15() {
+    close();
+    setiframe('https://code-live.ru/post/cpp-overloading-class-methods/');
+}
+
+function action_16() {
+    close();
+    setiframe('https://code-live.ru/post/cpp-operators/');
+}
+
+function cutFrame() {
+}
 
 function login() {
 	var login = document.getElementById('loginEdit').value;
@@ -7,7 +95,9 @@ function login() {
 }
 
 function register() {
-	alert('open register');
+	//alert('open register');
+    //var newWindow = window.open("","hello","width=200,height=200");
+    //window.location.href = "hello.html";
 }
 
 function listOfLessons() {
@@ -19,7 +109,6 @@ function listOfLessons() {
 
 function loginButton() {
     document.getElementById('reg_form').style.display = "block";
-    document.querySelector
 }
 
 function close() {
@@ -32,7 +121,7 @@ function ready() {
     close();
     document.getElementById('choose_a').style.display = 'block';
     var test_a = document.getElementById('test_a');
-        
+    var iframe = document.getElementsByTagName('iframe')[0];
     var innerDoc = (iframe.contentDocument) ? iframe.contentDocument : iframe.contentWindow.document;
                 
     //test_a.innerHTML = innerDoc;
